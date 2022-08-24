03d41c19db45e29990feb0bebfa2a49922d455af
Mario Cordoba <mario.cordoba@astrogrp.com.ni>
2022-08-24 11:02:33 -0600

    ttdrxd

--- AIP_SCP/VIEW/Form_Menu.Designer.cs
+++ AIP_SCP/VIEW/Form_Menu.Designer.cs
@@ -37,6 +37,7 @@ namespace AIP_SCP
             this.pERMISOSSISTEMAToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.uSUARIOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -105,11 +106,22 @@ namespace AIP_SCP
             this.label1.TabIndex = 2;
             this.label1.Text = "BIENVENIDO";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(313, 223);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(428, 73);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "BIENVENIDO";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1055, 518);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -132,6 +144,7 @@ namespace AIP_SCP
         private System.Windows.Forms.ToolStripMenuItem uSUARIOSToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem sALIRToolStripMenuItem1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
