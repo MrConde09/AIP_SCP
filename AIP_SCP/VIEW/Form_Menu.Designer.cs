bb54aa41def5f94ff89debd92ef492982a7b3501
Mario Cordoba <mario.cordoba@astrogrp.com.ni>
2022-08-24 11:10:11 -0600

    adfdf

--- AIP_SCP/VIEW/Form_Menu.Designer.cs
+++ AIP_SCP/VIEW/Form_Menu.Designer.cs
@@ -110,7 +110,7 @@ namespace AIP_SCP
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(313, 223);
+            this.label2.Location = new System.Drawing.Point(261, 277);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(428, 73);
             this.label2.TabIndex = 3;
